ee0b9dfe7bf63e5e233d179cce6b80cc4a12b206
danil <44bridgeprod@gmail.com>
2019-01-19 17:38:14 +0200

    still trying

--- Vanilla-JS--Chart-project/webpack.config.js
+++ Vanilla-JS--Chart-project/webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
         test: /\.css$/,
-        exclude: /node_modules/,
         use: [
           "style-loader",
           MiniCssExtractPlugin.loader,
