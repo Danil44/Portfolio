fae28d4708acbf576c930074dd70a9508335b6a7
danil <44bridgeprod@gmail.com>
2019-01-19 17:50:11 +0200

    and still trying

--- Vanilla-JS--Chart-project/src/index.js
+++ Vanilla-JS--Chart-project/src/index.js
@@ -6,6 +6,8 @@ import "./scss/components/chart.scss";
 import "./scss/components/charts-list.scss";
 import "./scss/components/interface.scss";
 import "./scss/components/table-section.scss";
+import "../node_modules/intro.js/introjs.css";
+import "../node_modules/codemirror-colorpicker/dist/codemirror-colorpicker.css";
 
 import Model from "./js/model";
 import View from "./js/view";
@@ -14,5 +16,3 @@ import Controller from "./js/controller";
 const model = new Model();
 const view = new View();
 new Controller(model, view);
-
-
